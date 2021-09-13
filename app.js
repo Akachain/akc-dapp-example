8a71fbac586def7db0c6c7ef46937ada3ee17f40
thienlk1 <thienlk0901@gmail.com>
2021-09-13 21:21:03 +0700

    fix BFS

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const job = require('./src/job');
 
 // const tx = require('./src/utxo.js');
 
-var rawData = [
+// var rawData = [
     // { RequestId: '1', TransactionType: 'Mint', Transfer: [{ From: 'Operator', To: 'IAO', TokenId: "ST", Amount: 10000 }], Batch: null, Checked: false },
     // { RequestId: '2', TransactionType: 'Deposit', Transfer: [{ From: 'Alice', To: 'Bob', TokenId: "ST", Amount: 1000 }], Batch: null, Checked: false },
     // { RequestId: '3', Transaction_Type: 'Exchange', Transfer: [{ From: 'Bob', To: 'IAO', TokenId: "ST", Amount: 200 }, { From: 'IAO', To: 'Hold', TokenId: "AT1", Amount: 20 }], Batch: null, Checked: false },
-    { RequestId: '4', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'XYZ', TokenId: "ST", Amount: 15 },{ From: 'Alice', To: 'XYZ', TokenId: "ST", Amount: 10 },{ From: 'Alice', To: 'Bob', TokenId: "ST", Amount: 1 }], Batch: null, Checked: false },
-    { RequestId: '5', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'Bob', TokenId: "AT2", Amount: 1 }], Batch: null, Checked: false },
-    { RequestId: '6', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'Bob', TokenId: "AT2", Amount: 2 }], Batch: null, Checked: false },
-    { RequestId: '7', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'Bob', TokenId: "AT2", Amount: 3 }], Batch: null, Checked: false },
-    { RequestId: '8', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'Bob', TokenId: "AT2", Amount: 4 }], Batch: null, Checked: false },
-];
+    // { RequestId: '4', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'XYZ', TokenId: "ST", Amount: 15 },{ From: 'Alice', To: 'XYZ', TokenId: "ST", Amount: 10 },{ From: 'Alice', To: 'Bob', TokenId: "ST", Amount: 1 }], Batch: null, Checked: false },
+    // { RequestId: '5', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'Bob', TokenId: "AT2", Amount: 1 }], Batch: null, Checked: false },
+    // { RequestId: '6', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'Bob', TokenId: "AT2", Amount: 2 }], Batch: null, Checked: false },
+    // { RequestId: '7', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'Bob', TokenId: "AT2", Amount: 3 }], Batch: null, Checked: false },
+    // { RequestId: '8', Transaction_Type: 'Transfer', Transfer: [{ From: 'Alice', To: 'Bob', TokenId: "AT2", Amount: 4 }], Batch: null, Checked: false },
+// ];
 // var txs = [
 //     { FromWallet: 'barney', ToWallet: 'Alice', FromTokenId: 'ST', ToTokenId: "AT1", FromTokenAmount: 1500, ToTokenAmount: 1500, TxType: "Burn", Status: "RECEIVE" },
 //     { FromWallet: 'barney', ToWallet: 'Alice', FromTokenId: 'ST', ToTokenId: "AT1", FromTokenAmount: 1500, ToTokenAmount: 1500, TxType: "Burn", Status: "RECEIVE" },
@@ -22,5 +22,5 @@ var rawData = [
 //     { FromWallet: 'barney', ToWallet: 'Alice', FromTokenId: 'ST', ToTokenId: "AT1", FromTokenAmount: 1500, ToTokenAmount: 1500, TxType: "Burn", Status: "RECEIVE" },
 // ];
 
-job.callTxOnchain(rawData);
+// job.callTxOnchain(rawData);
 // job.callMintOnchain(rawData);
